e5ee9864ac69e940f3796903d72ea3ede8ae3a42
Josuerodrigojr <98060884+Josuerodrigojr@users.noreply.github.com>
2023-09-14 18:49:16 -0300

    Configuração de cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const rotas = require("./rotas")
 require('dotenv').config()
 
+app.use(cors())
 app.use(express.json())
 app.use(rotas)
 
